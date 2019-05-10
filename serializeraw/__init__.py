e920a2e5e4e2b9503b036dba281c74c72f78befc
Helmut Konrad Fahrendholz <kiwi@derspanier.de>
2019-05-10 14:48:59 +0200

    feat(hits): add load and dump method to serialize border hits

--- serializeraw/__init__.py
+++ serializeraw/__init__.py
@@ -17,6 +17,9 @@ from serializeraw.border import dump_boundingboxes
 from serializeraw.border import dump_pageborders
 from serializeraw.border import load_boundingboxes
 from serializeraw.border import load_pageborders
+# hits
+from serializeraw.hits import dump_hits
+from serializeraw.hits import load_hits
 # document
 from serializeraw.text import dump_yaml as dump_document
 from serializeraw.text import load_yaml as load_document
---
+++ serializeraw/hits.py
@@ -0,0 +1,46 @@
+# =============================================================================
+# C O P Y R I G H T
+# -----------------------------------------------------------------------------
+# Copyright (c) 2019 by Helmut Konrad Fahrendholz. All rights reserved.
+# This file is property of Helmut Konrad Fahrendholz. Any unauthorized copy,
+# use or distribution is an offensive act against international law and may
+# be prosecuted under federal law. Its content is company confidential.
+# =============================================================================
+
+from utila import from_raw_or_path
+from yaml import FullLoader
+from yaml import dump
+from yaml import load
+
+from serializeraw.border import border_fromraw
+
+
+def dump_hits(border, content):
+    # content: page, index, current
+    result = [
+        '%d %d %.2f %.2f %.2f %.2f' % (page, index, *current)
+        for page, index, current in content
+    ]
+    raw = {
+        'border': '%.2f %.2f %.2f %.2f' % border,
+        'hits': result,
+    }
+    return dump(raw)
+
+
+def load_hits(content):
+    content = from_raw_or_path(content, ftype='yaml')
+    loaded = load(content, Loader=FullLoader)
+
+    border = border_fromraw(loaded['border'])
+    hits_raw = loaded['hits']
+
+    hits = []
+    for hit in hits_raw:
+        splitted = hit.split(' ', maxsplit=2)
+        page = int(splitted[0])
+        index = int(splitted[1])
+        box = border_fromraw(splitted[2])
+
+        hits.append((page, index, box))
+    return border, hits
