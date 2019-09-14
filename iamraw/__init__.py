44faebe8e3c62bbae3bbeb9c95817fdde47c8464
Helmut Konrad Fahrendholz <kiwi@derspanier.de>
2019-09-14 10:17:37 +0200

    1.8.4 automatically generated release

--- iamraw/__init__.py
+++ iamraw/__init__.py
@@ -96,6 +96,6 @@ from iamraw.toc import Section
 from iamraw.toc import Toc
 from iamraw.toc import create_toc
 
-__version__ = '1.8.3'
+__version__ = '1.8.4'
 
 ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
