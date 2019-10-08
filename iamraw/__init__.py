43b92543101a626293351ff2485972c7bc1143d5
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2019-10-08 16:10:25 +0200

    1.8.10 automatically generated release

--- iamraw/__init__.py
+++ iamraw/__init__.py
@@ -97,6 +97,6 @@ from iamraw.toc import Section
 from iamraw.toc import Toc
 from iamraw.toc import create_toc
 
-__version__ = '1.8.9'
+__version__ = '1.8.10'
 
 ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
