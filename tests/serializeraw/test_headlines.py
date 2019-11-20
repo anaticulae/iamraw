33b6fa5ca21a23d8d0ee7f8771e330748e538dfc
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2019-11-20 07:30:30 +0100

    style(tests): organize imports

--- tests/serializeraw/test_headlines.py
+++ tests/serializeraw/test_headlines.py
@@ -6,20 +6,19 @@
 # use or distribution is an offensive act against international law and may
 # be prosecuted under federal law. Its content is company confidential.
 # =============================================================================
-from iamraw import Headline
-from serializeraw import dump_headlines
-from serializeraw import load_headlines
+import iamraw
+import serializeraw
 
 EXPECTED = [
     [
-        Headline(
+        iamraw.Headline(
             text='CHAPTER 1',
             level=1,
             rawlevel='1',
             container=0,
             page=6,
         ),
-        Headline(
+        iamraw.Headline(
             text='RestructuredText Tutorial',
             level=2,
             rawlevel='',
@@ -28,35 +27,35 @@ EXPECTED = [
         ),
     ],
     [
-        Headline(
+        iamraw.Headline(
             text='CHAPTER 2',
             level=1,
             rawlevel='2',
             container=0,
             page=8,
         ),
-        Headline(
+        iamraw.Headline(
             text='RestructuredText Guide',
             level=2,
             rawlevel='',
             container=1,
             page=8,
         ),
-        Headline(
+        iamraw.Headline(
             text='Basics',
             level=3,
             rawlevel='',
             container=2,
             page=8,
         ),
-        Headline(
+        iamraw.Headline(
             text='Blockquotes',
             level=3,
             rawlevel='',
             container=1,
             page=9,
         ),
-        Headline(
+        iamraw.Headline(
             text='Code: Block',
             level=3,
             rawlevel='',
@@ -69,10 +68,10 @@ EXPECTED = [
 
 def test_headlines_dump_and_load_headlines():
     """Dump and load the example above"""
-    dumped = dump_headlines(EXPECTED)
-    loaded = load_headlines(dumped)
+    dumped = serializeraw.dump_headlines(EXPECTED)
+    loaded = serializeraw.load_headlines(dumped)
 
     assert loaded == EXPECTED
 
-    loaded = load_headlines(dumped, tuple(range(10)))
+    loaded = serializeraw.load_headlines(dumped, tuple(range(10)))
     assert len(loaded) == 2
