0d2939b44f97ac5a3af148d91ee6ef42b5eb2ee9
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2020-01-11 20:31:12 +0100

    1.12.10 automatically generated release

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,8 @@
 
 Every noteable change is logged here.
 
+## v1.12.10
+
 ## v1.12.9
 
 ### Feature
--- iamraw/__init__.py
+++ iamraw/__init__.py
@@ -124,6 +124,6 @@ from iamraw.whitepage import PageContentWhitepage
 from iamraw.whitepage import PageContentWhitepages
 from iamraw.whitepage import WhitePage
 
-__version__ = '1.12.9'
+__version__ = '1.12.10'
 
 ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
