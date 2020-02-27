d75d72f176aec8a78d04b70688549b9e8207ba66
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2020-02-27 19:05:41 +0100

    test(resources): add example to verify pagtextnavigator

--- tests/serializeraw/__init__.py
+++ tests/serializeraw/__init__.py
@@ -6,6 +6,7 @@
 # use or distribution is an offensive act against international law and may
 # be prosecuted under federal law. Its content is company confidential.
 # =============================================================================
+import os
 from os.path import exists
 from os.path import join
 
@@ -21,3 +22,16 @@ assert exists(TEXT_YAML), TEXT_YAML
 
 TOC_YAML = join(DATA, 'toc.yaml')
 assert exists(TOC_YAML), TOC_YAML
+
+RESTRUCTURED = os.path.join(DATA, 'restructured')
+
+RESOURCES = [
+    TEST,
+    DATA,
+    TEXT_YAML,
+    TOC_YAML,
+    RESTRUCTURED,
+]
+
+for item in RESOURCES:
+    assert os.path.exists(item), str(item)
