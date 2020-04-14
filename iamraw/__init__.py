479d40d2cce54bed0d100a8bf12c9f9dd7444f79
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2020-04-14 13:18:44 +0200

    1.22.3 automatically generated release

--- iamraw/__init__.py
+++ iamraw/__init__.py
@@ -168,6 +168,6 @@ from iamraw.whitepage import PageContentWhitepage
 from iamraw.whitepage import PageContentWhitepages
 from iamraw.whitepage import WhitePage
 
-__version__ = '1.22.2'
+__version__ = '1.22.3'
 
 ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
