752271d57182b8e460b26505eb4307c6210702a8
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2020-08-01 20:23:52 +0200

    docs(formula): add formula dump and load test

--- serializeraw/formula.py
+++ serializeraw/formula.py
@@ -6,13 +6,30 @@
 # use or distribution is an offensive act against international law and may
 # be prosecuted under federal law. Its content is company confidential.
 # =============================================================================
+"""Extracted Raw Formulas
+======================
+
+>>> EXTRACTED = [iamraw.PageContentFormula(page=5, content=[iamraw.FormulaRaw(
+...     page=5,
+...     content=[
+...         iamraw.MathChar(bounding=(491.2, 109.97, 495.18, 124.63), size=14.66, value='x'),
+...         iamraw.MathChar(bounding=(495.18, 109.97, 501.15, 124.63), size=14.66, value='='),
+...         iamraw.MathChar(bounding=(501.15, 109.97, 504.14, 124.63), size=14.66, value='y'),
+...         iamraw.MathChar(bounding=(504.14, 109.97, 510.12, 124.63), size=14.66, value='+'),
+...         iamraw.MathChar(bounding=(510.12, 109.97, 514.1, 124.63), size=14.66, value='3')
+...     ]
+... )])]
+
+>>> dumped = dump_rawformulas(EXTRACTED)
+>>> loaded = load_rawformulas(dumped)
+>>> assert loaded == EXTRACTED
+
+"""
 
 import utila
 import yaml
 
 import iamraw
-
-# TODO: ADD UNIT TEST
 
 
 def dump_formulas(pages: iamraw.PageContentFormula) -> str:
