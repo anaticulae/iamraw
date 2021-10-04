84a00c40dc6d5ac83cd7340f4729fed4ba0168d8
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2021-10-04 11:04:48 +0200

    feat(images): add attribute to store image hash value

--- iamraw/images.py
+++ iamraw/images.py
@@ -6,6 +6,13 @@
 # use or distribution is an offensive act against international law and may
 # be prosecuted under federal law. Its content is company confidential.
 # =============================================================================
+"""\
+Compare ImageInformation `hashedimage` independent:
+>>> hash(ImageInformation(hashedimage='first')) == hash(ImageInformation(hashedimage='second'))
+True
+>>> ImageInformation(hashedimage='first') == ImageInformation(hashedimage='second')
+True
+"""
 
 import collections
 import dataclasses
@@ -18,13 +25,14 @@ PageContentImageInfo = collections.namedtuple(
 PageContentImageInfos = typing.List[PageContentImageInfo]
 
 
-@dataclasses.dataclass
+@dataclasses.dataclass(unsafe_hash=True)
 class ImageInformation:
     width: int = None
     height: int = None
     page: int = None
     dpi: tuple = None
     bounding: tuple = None
+    hashedimage: str = dataclasses.field(default=None, hash=None, compare=False)
     hidden: bool = False
     """Disable image to avoid further processing."""
 
