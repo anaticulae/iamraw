53e971ccd6e1f8b3571d387455ec8b8a3b2785ed
Helmut Konrad Schewe <helmutus@outlook.com>
2026-07-07 00:56:07 +0200

    test(header): enable unit tests

--- tests/serializeraw/test_headerfooter.py
+++ tests/serializeraw/test_headerfooter.py
@@ -145,7 +145,6 @@ def test_dump_mergednote():
     assert hash(str(data)) == before, 'data changed due simplify'
 
 
-@pytest.mark.xfail(reason='enable groupme')
 @utilotest.requires(hoverpower.MASTER072_PDF)
 def test_footer_load():
     source = hoverpower.link(hoverpower.MASTER072_PDF)
@@ -153,7 +152,6 @@ def test_footer_load():
     assert utilo.near(current=len(loaded), expected=7, diff=3)  # seven pages
 
 
-@pytest.mark.xfail(reason='enable groupme')
 @utilotest.requires(hoverpower.MASTER072_PDF)
 def test_footnotes_load():
     source = hoverpower.link(hoverpower.MASTER072_PDF)
